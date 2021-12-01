4027a6771e59772f57017de669a38be6677d7878
janic0 <prodjnc@gmail.com>
2021-12-01 15:11:11 +0100

    added bind server to 0.0.0.0

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,6 +6,6 @@ app.get("/ping", (_, res) => {
   res.send("pong")
 })
 
-app.listen(process.env.PORT || 5000, (err, address) => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", (err, address) => {
   console.log(err, address)
 })
