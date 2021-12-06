5abb3393a539d73d5ce2d05aff4c8cd3e3a7669d
janic0 <prodjnc@gmail.com>
2021-12-06 23:01:02 +0100

    fixed imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import './assets/scss/base.scss'
-import './assets/js/appearence'
-import darkIcon from './assets/media/darkmode.svg';
-import lightIcon from './assets/media/lightmode.svg';
-import Default from './template/default'
-import Welcome from './template/view/welcome';
-import Signin from './template/view/signin';
-import Signup from './template/view/signup';
-import Plans from './template/view/plans';
+import "./App.css";
+import "./assets/scss/base.scss";
+import "./assets/scss/files/layout.scss";
+import "./assets/js/appearence";
+import darkIcon from "./assets/media/darkmode.svg";
+import lightIcon from "./assets/media/lightmode.svg";
+import Default from "./template/default";
+import Welcome from "./template/view/welcome";
+import Signin from "./template/view/signin";
+import Signup from "./template/view/signup";
+import Plans from "./template/view/plans";
 
 function App(props) {
-  return (
-    <div className="App" data-appearance="" data-appearance-invertet="false">
-        {
-          props.view.map((e) => {
-            // return <Default media={[darkIcon, lightIcon]}></Default>;
-            return <Plans></Plans>;
-          })
-        }
-    </div>
-  );
+	return (
+		<div className="App" data-appearance="" data-appearance-invertet="false">
+			{props.view.map((e) => {
+				// return <Default media={[darkIcon, lightIcon]}></Default>;
+				return <Plans></Plans>;
+			})}
+		</div>
+	);
 }
 
 export default App;
