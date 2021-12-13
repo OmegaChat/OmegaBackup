581c7fbe542a1e301bbd0e7d0649bae879f244e0
janic0 <prodjnc@gmail.com>
2021-12-13 09:27:47 +0100

    added signedIn and signUp test

--- frontend/src/template/view/look.jsx
+++ frontend/src/template/view/look.jsx
@@ -1,7 +1,19 @@
 import "../../assets/scss/files/look.scss";
 import logoWhite from "../../assets/media/logowhite.svg";
+import { useEffect, useState } from "react";
+import backend from "../shared/url";
 
 const Look = () => {
+	useEffect(() => {
+		fetch(backend + "/v1/user/loginStatus").then((d) => {
+			if (d.status === 200) {
+				setLoggedIn(true);
+			} else {
+				setLoggedIn(false);
+			}
+		});
+	}, []);
+	const [loggedIn, setLoggedIn] = useState(false);
 	return (
 		<div className="look">
 			<div className="look__logo">
@@ -9,26 +21,31 @@ const Look = () => {
 			</div>
 			<h1 className="look__title">Welcome to OmegaBackup</h1>
 			<p className="look__todo">
-				You just created an account on the best Backup Platform on earth.
+				{loggedIn
+					? "You just created an account on the best Backup Platform on earth."
+					: "You are not currently logged in."}
 				<br />
 				<br />
-				Start by{" "}
-				<a className="todo__link" href="/download">
-					downloading our client{" "}
+				{loggedIn ? "Start by " : "Sign Up using "}
+				<a className="todo__link" href={loggedIn ? "/download" : "/signup"}>
+					{loggedIn ? "downloading our client" : "this link"}
 				</a>
-				or proceed to
-				<a className="todo__link" href="/app">
-					{" "}
-					view your backups
+				{loggedIn ? " or proceed to " : " or log in "}
+				<a className="todo__link" href={loggedIn ? "/app" : "/signup"}>
+					{loggedIn ? " view your backups" : "using this link."}
 				</a>
 				.
 			</p>
 			<div className="look__buttons">
-				<a className="buttons__link" href="/download">
-					<button className="buttons__button">Download Client</button>
+				<a className="buttons__link" href={loggedIn ? "/download" : "/signup"}>
+					<button className="buttons__button">
+						{loggedIn ? "Download Client" : "Sign Up"}
+					</button>
 				</a>
-				<a className="buttons__link" href="/app">
-					<button className="buttons__button">View Backups</button>
+				<a className="buttons__link" href={loggedIn ? "/app" : "/signin"}>
+					<button className="buttons__button">
+						{loggedIn ? "View Backups" : "Sign In"}
+					</button>
 				</a>
 			</div>
 		</div>
