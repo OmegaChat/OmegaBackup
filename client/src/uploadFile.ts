433e3e4f83378744d0c969512ece94f47344363a
janic0 <prodjnc@gmail.com>
2021-12-14 10:37:25 +0100

    added better bundle

--- client/src/uploadFile.ts
+++ client/src/uploadFile.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { Buffer } from "buffer";
 
+const replaceAll = (str: string, find: string, target: string) => {
+	str.split(find).join(target);
+};
+
 export default (
 	pathName: string,
 	type: String,
@@ -11,7 +15,7 @@ export default (
 		.post(
 			process.argv[4] + "/v1/file/upload",
 			{
-				path: pathName,
+				path: replaceAll(pathName, "\\", "/"),
 				type: type,
 				data: data.toString("base64"),
 			},
